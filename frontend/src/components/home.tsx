62cabb5efd0cb12c67e6d3a9a7a5c288f2599100
Naoto Morishita <mori.nto@outlook.jp>
2025-11-08 23:10:33 +0900

    feat: Imageページの実装

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -3,7 +3,7 @@ import image from "@/assets/exit.svg";
 export const Home = () => {
   return (
     <div className="w-full grid place-items-center px-10">
-      <div className="w-full max-w-[580px] h-[380px] rounded-md bg-[#FFFFFF] dark:bg-[#212936] shadow-2xl p-3">
+      <div className="w-full max-w-[640px] aspect-[16/9] rounded-md bg-[#FFFFFF] dark:bg-[#212936] shadow-2xl p-3">
         <div className="border border-dashed border-[#4D5562] w-full h-full rounded-md flex flex-col items-center justify-center gap-y-3">
           <img className="w-8" src={image} alt="image upload icon" />
           <div className="text-sm">
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Layout } from "./components/layout";
 import { Home } from "./components/home";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { ImagePage } from "./components/image";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            {/* <Route path="images" element={<ImagePage />} /> */}
+            <Route path="image/:id" element={<ImagePage />} />
           </Route>
         </Routes>
       </BrowserRouter>
